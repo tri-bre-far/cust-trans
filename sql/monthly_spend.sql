d44afad35d83e7750a1c85d9339d1b1fbed58695
tri-bre-far <patsyfarq@hotmail.com>
2025-09-26 00:04:22 +0100

    Update monthly_spend.sql

--- sql/monthly_spend.sql
+++ sql/monthly_spend.sql
@@ -1,10 +1,13 @@
-CREATE OR REPLACE TABLE customer_analytics.monthly_spend
-PARTITION BY month
+CREATE OR REPLACE TABLE data-engineer-task-python.customer_analytics.monthly_spend
+PARTITION BY DATE_TRUNC(transaction_date, MONTH)
 CLUSTER BY customer_id AS
 SELECT
   customer_id,
-  FORMAT_TIMESTAMP('%Y-%m', transaction_date) AS month,
+  FORMAT_DATE('%Y-%m', transaction_date) AS month,
   SUM(amount) AS total_spend,
-  AVG(amount) AS avg_spend
-FROM customer_analytics.transactions
-GROUP BY customer_id, month;
+  AVG(amount) AS avg_spend,
+  transaction_date
+FROM
+  data-engineer-task-python.customer_analytics.transactions_deduped
+GROUP BY
+  customer_id, month, transaction_date;
