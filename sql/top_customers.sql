3116c8dbea2cde94dd304430de34cf6244cfb81c
tri-bre-far <patsyfarq@hotmail.com>
2025-09-26 00:05:07 +0100

    Update top_customers.sql

--- sql/top_customers.sql
+++ sql/top_customers.sql
@@ -1,7 +1,11 @@
-CREATE OR REPLACE TABLE customer_analytics.top_customers
+CREATE OR REPLACE TABLE data-engineer-task-python.customer_analytics.top_customers
 CLUSTER BY customer_id AS
 SELECT
   customer_id,
   SUM(amount) AS lifetime_spend
-FROM customer_analytics.transactions
-GROUP BY customer_id;
+FROM
+  data-engineer-task-python.customer_analytics.transactions_deduped
+GROUP BY
+  customer_id
+QUALIFY
+  PERCENT_RANK() OVER (ORDER BY SUM(amount) DESC) <= 0.05;
